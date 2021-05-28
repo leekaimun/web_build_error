b92c5f45c651f4e52ea1b56ea11f7f4e39a3228b
leekaimun <leekaimun@gmail.com>
2021-05-29 00:01:33 +0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flexible Space',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyTabbedPage(),
+    );
+  }
+}
+
+class MyTabbedPage extends StatelessWidget {
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'RIGHT'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(color: Colors.amber),
+          bottom: TabBar(tabs: myTabs),
+        ),
+        body: TabBarView(
+          children: myTabs.map((Tab tab) {
+            final String label = tab.text!.toLowerCase();
+            return Center(
+              child: Text(
+                'This is the $label tab',
+                style: const TextStyle(fontSize: 36),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
